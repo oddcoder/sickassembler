3381f8f1df71d4fda428bdf0a01e0c0b1cd6cbf8
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2017-03-17 13:32:19 +0200

    Remove unused test

--- src/lib/pass_two/tests.rs
+++ src/lib/pass_two/tests.rs
@@ -1,11 +1,2 @@
 #[cfg(test)]
-mod tests {
-    //    use super::*;
-
-    #[test]
-    #[should_panic]
-    fn it_panics() {
-        println!("I'll die now");
-        panic!("aaahh");
-    }
-}
+mod pass_two_tests {}
