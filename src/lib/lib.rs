b8a8208a553bdfc54431321cdc8509cb50547a00
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2017-03-21 21:20:52 +0200

    reset last commit

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,8 +1,11 @@
 /**
  * Higher modules will be declared here
  */
+<<<<<<< 92f6f67d1d8d320e3c915ee6118383b6f5b79e86
 #[macro_use]
 extern crate lazy_static;
+=======
+>>>>>>> reset last commit
 pub mod basic_types;
 pub mod pass_two;
 pub mod filehandler;
