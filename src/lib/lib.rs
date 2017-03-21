e1d473ff8111acceb26c0be986601ad7d617eea5
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2017-03-21 21:21:26 +0200

    Mistakenly deleted lazy_static
    This reverts commit 9924be981a1b4ecf0fcb8f7a3b4e586ea436a6d7.

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,6 +1,12 @@
 /**
  * Higher modules will be declared here
  */
+#[macro_use]
+extern crate lazy_static;
+
+#[macro_use]
+extern crate bitflags;
+
 pub mod basic_types;
 pub mod pass_two;
 pub mod filehandler;
