10dc9c6fc35de20623b71fb02e241968ea5fc185
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2017-03-21 21:23:04 +0200

    remove bitflags

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,20 +1,9 @@
 /**
  * Higher modules will be declared here
  */
-<<<<<<< cc8984b553db19d87f1892c8faf3faaa1dc971ae
-<<<<<<< 92f6f67d1d8d320e3c915ee6118383b6f5b79e86
-#[macro_use]
-extern crate lazy_static;
-=======
->>>>>>> reset last commit
-=======
 #[macro_use]
 extern crate lazy_static;
 
-#[macro_use]
-extern crate bitflags;
-
->>>>>>> Mistakenly deleted lazy_static
 pub mod basic_types;
 pub mod pass_two;
 pub mod filehandler;
