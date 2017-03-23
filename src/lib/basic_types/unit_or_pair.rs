4f351606a63804709ed42c8f4baf48e9accf1c77
oddcoder <ahmedsoliman@oddcoder.com>
2017-03-23 17:25:11 +0200

    Data entry task ... /o\
    Adding isa data

--- src/lib/basic_types/unit_or_pair.rs
+++ src/lib/basic_types/unit_or_pair.rs
@@ -1,8 +1,9 @@
 // TODO use this instead of the tuple in the AssemblyDefs, later
 // this type is created to avoid having a tuple with an empty entry incase only one operand
 // is present
-#[derive(Debug,PartialEq)]
+#[derive(Debug,PartialEq,Clone)]
 pub enum UnitOrPair<T> {
     Unit(T),
     Pair(T, T),
+    None,
 }
