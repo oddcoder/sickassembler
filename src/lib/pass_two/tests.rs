46c978cf8a124e5eaefd7b5cf0c817d6795e4a01
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2017-03-24 10:23:30 +0200

    Update tests

--- src/lib/pass_two/tests.rs
+++ src/lib/pass_two/tests.rs
@@ -1,27 +1,22 @@
-
-
-
 use pass_two::translator::*;
-use basic_types::instruction::Instruction;
-use basic_types::operands::OperandType;
+use basic_types::instruction::{Instruction, AsmOperand};
+use basic_types::operands::{OperandType, Value};
 use basic_types::formats::Format;
 use basic_types::flags::Flags;
 use basic_types::unit_or_pair::UnitOrPair;
 
 #[test]
 fn flag_resolution() {
-    OperandType::Immediate(Some(6));
     //pass_two::object_code_generator::object_code_gen::generate_object_code
 
-    let mut instr: Instruction =
-        Instruction::new(String::new(),
-                         "load".to_owned(),
-                         UnitOrPair::Pair(OperandType::Immediate(Some(5)),
-                                          OperandType::Immediate(Some(1))));
+    let mut instr: Instruction = Instruction::new_simple("load".to_owned());
+
+    instr.add_operand(OperandType::Immediate, Value::SignedInt(5));
 
     instr.set_format(Format::Four);
     instr.set_flag(Flags::Immediate);
     instr.set_flag(Flags::Extended);
 
-    //generate_object_code(instr);
+    // n i x b p e | 20-addr - 0 - indexed
+    assert_eq!(instr.get_flags_value().unwrap(), (1 << 20 + 4) + (1 << 20));
 }
