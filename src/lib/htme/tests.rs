5e3492fdd42538ec941d8e3115d631af1b4ece07
Youssefares <Youssefares@ymail.com>
2017-03-25 17:17:25 +0200

    string_from_code now fully tested

--- src/lib/htme/tests.rs
+++ src/lib/htme/tests.rs
@@ -10,19 +10,60 @@ mod formatting_tests{
         let string_code = match string_from_object_code(code, Format::One){
             Ok(s) =>  s,
             Err(e) => {
-                println!("Error: {:}", e);
+                panic!("Error: {:}", e);
                 "".to_string()
             }
         };
         assert_eq!("C4".to_string(), string_code);
 
-        // let code: i32 = 243;
-        // let string_code = string_from_object_code(code, Format::Two);
-        // assert_eq!("00F3".to_string(), string_code);
-        //
-        // let code: i32 = 259283;
-        // let string_code = string_from_object_code(code, Format::Four);
-        // assert_eq!("0003F4D3".to_string(), string_code);
+
+        let code: i32 = 243;
+        let string_code = match string_from_object_code(code, Format::Two){
+            Ok(s) =>  s,
+            Err(e) => {
+                println!("Error: {:}", e);
+                "".to_string()
+            }
+        };
+        assert_eq!("00F3".to_string(), string_code);
+
+
+        let code: i32 = 259283;
+        let string_code = match string_from_object_code(code, Format::Four){
+            Ok(s) =>  s,
+            Err(e) => {
+                println!("Error: {:}", e);
+                "".to_string()
+            }
+        };
+        assert_eq!("0003F4D3".to_string(), string_code);
+    }
+
+    #[test]
+    #[should_panic]
+    fn failed_test_string_from_code(){
+
+        //should panic: format = None
+        let code: i32 = 243;
+        let string_code = match string_from_object_code(code, Format::None){
+            Ok(s) =>  s,
+            Err(e) => {
+                panic!("Error: {:}", e);
+                "".to_string()
+            }
+        };
+        assert_eq!("".to_string(), string_code);
+
+        //should panic: size of format < size of code
+        let code: i32 = 259283;
+        let string_code = match string_from_object_code(code, Format::One){
+            Ok(s) =>  s,
+            Err(e) => {
+                panic!("Error: {:}", e);
+                "".to_string()
+            }
+        };
+        assert_eq!("".to_string(), string_code);
 
     }
 
