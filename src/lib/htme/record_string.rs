26ef54b37f7d09add0baa49bb13c64a69eb129cd
Youssefares <Youssefares@ymail.com>
2017-03-25 19:06:39 +0200

    added text_record form program array-of-tuple model, also removed a lot of unreachable testing code

--- src/lib/htme/record_string.rs
+++ src/lib/htme/record_string.rs
@@ -1,6 +1,30 @@
 use basic_types::formats::Format;
 
 
+/*
+ * returns string containing all records from program if all code and format pair make sense.
+ * returns error message and its index for an invalid code and format.
+ * TODO: format record well
+ */
+
+pub fn text_record_from_program(program: &Vec<(i32, Format)>)->Result<String, (&'static str, i32)>{
+    let mut i = 0;
+    let mut record = String::from("");
+    for &(code, format) in program.iter(){
+        match string_from_object_code(code,format) {
+            Ok(s) => record.push_str(&s),
+            Err(s) => return Err((s,i))
+        }
+        i = i+1;
+    }
+    return Ok(record);
+}
+
+
+/*
+ * returns string from a format-valid object-code.
+ * returns error-string if the object-code and format don't add up.
+ */
 pub fn string_from_object_code(code: i32, format: Format)-> Result<String, &'static str> {
     let string_width_in_bytes = format as u8;
     if format == Format::None{
@@ -13,6 +37,10 @@ pub fn string_from_object_code(code: i32, format: Format)-> Result<String, &'sta
     return Ok(format!("{:01$X}", code, hex_digits as usize));
 }
 
+
+/*
+ * returns i32 minimum number of hexa digits required to store the i32 code.
+ */
 pub fn min_hexa_digits_required(code:i32)-> i32 {
     return (code as f64).log(16.0) as i32 + 1;
 }
--- src/lib/htme/tests.rs
+++ src/lib/htme/tests.rs
@@ -17,17 +17,6 @@ mod formatting_tests{
         assert_eq!("C4".to_string(), string_code);
 
 
-        let code: i32 = 243;
-        let string_code = match string_from_object_code(code, Format::Two){
-            Ok(s) =>  s,
-            Err(e) => {
-                println!("Error: {:}", e);
-                "".to_string()
-            }
-        };
-        assert_eq!("00F3".to_string(), string_code);
-
-
         let code: i32 = 259283;
         let string_code = match string_from_object_code(code, Format::Four){
             Ok(s) =>  s,
@@ -49,10 +38,8 @@ mod formatting_tests{
             Ok(s) =>  s,
             Err(e) => {
                 panic!("Error: {:}", e);
-                "".to_string()
             }
         };
-        assert_eq!("".to_string(), string_code);
 
         //should panic: size of format < size of code
         let code: i32 = 259283;
@@ -60,10 +47,8 @@ mod formatting_tests{
             Ok(s) =>  s,
             Err(e) => {
                 panic!("Error: {:}", e);
-                "".to_string()
             }
         };
-        assert_eq!("".to_string(), string_code);
 
     }
 
@@ -74,5 +59,38 @@ mod formatting_tests{
 
         let digits = min_hexa_digits_required(259283);
         assert_eq!(digits, 5);
+    }
+
+
+    #[test]
+    fn test_text_record_from_program(){
+        let valid_program = vec![(196, Format::One), (243, Format::Two), (259283, Format::Four)];
+        let record = match text_record_from_program(&valid_program){
+            Ok(s) => {
+                s
+            },
+            Err((err, i)) => {
+                panic!("Error: {:} at code with index {}", err,i);
+            }
+        };
+        assert_eq!(record, String::from("C400F30003F4D3"));
+    }
+
+
+
+
+    #[test]
+    #[should_panic]
+    fn failed_test_record_from_program() {
+        let valid_program = vec![(196, Format::One), (243, Format::None), (259283, Format::Four)];
+        let record = match text_record_from_program(&valid_program){
+            Ok(s) => {
+                println!("{:}", s);
+                s
+            },
+            Err((err, i)) => {
+                panic!("Error: {:} at code with index {}", err,i);
+            }
+        };
     }
 }
