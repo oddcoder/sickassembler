97cf2c72459217fd461e9aca936ce7bed9b39453
Youssefares <Youssefares@ymail.com>
2017-03-25 22:14:59 +0200

    refactored tests and structured the raw_program model

--- src/lib/htme/mod.rs
+++ src/lib/htme/mod.rs
@@ -3,4 +3,5 @@
  */
 
 pub mod record_string;
+pub mod raw_program;
 pub mod tests;
--- src/lib/htme/record_string.rs
+++ src/lib/htme/record_string.rs
@@ -1,5 +1,6 @@
 use basic_types::formats::Format;
 
+//TODO: decide if I need to move these?
 
 /*
  * returns string containing all records from program if all code and format pair make sense.
@@ -7,17 +8,27 @@ use basic_types::formats::Format;
  * TODO: format record well
  */
 
-pub fn text_record_from_program(program: &Vec<(i32, Format)>)->Result<String, (&'static str, i32)>{
+pub fn text_record_from_program(program: &Vec<(u32, u32, Format)>)->Result<String, (&'static str, i32)>{
+
+    //counter to know where error happened
     let mut i = 0;
+    let record_type = String::from("T");
+    //empty record we keep appending to
     let mut record = String::from("");
-    for &(code, format) in program.iter(){
+
+    //iterate on program (code-format tuple)
+    for &(address, code, format) in program.iter(){
+        //if string push it, if error, return (error, index)
         match string_from_object_code(code,format) {
             Ok(s) => record.push_str(&s),
             Err(s) => return Err((s,i))
         }
+        //next
         i = i+1;
     }
-    return Ok(record);
+
+    //if we're here, everything went well and record is returned
+    return Ok(record_type + &record);
 }
 
 
@@ -25,22 +36,26 @@ pub fn text_record_from_program(program: &Vec<(i32, Format)>)->Result<String, (&
  * returns string from a format-valid object-code.
  * returns error-string if the object-code and format don't add up.
  */
-pub fn string_from_object_code(code: i32, format: Format)-> Result<String, &'static str> {
+pub fn string_from_object_code(code: u32, format: Format)-> Result<String, &'static str> {
     let string_width_in_bytes = format as u8;
+
+    //error checking
     if format == Format::None{
         return Err("argument format equals None. Init your sick instruction well!")
     }
-    let hex_digits = (string_width_in_bytes*2) as i32;
+    let hex_digits = (string_width_in_bytes*2) as u32;
     if hex_digits < min_hexa_digits_required(code){
         return Err("object code value is too big for format size. What the heck are you doing?")
     }
+
+    //return string with right amount of zeros to the left.
     return Ok(format!("{:01$X}", code, hex_digits as usize));
 }
 
 
 /*
- * returns i32 minimum number of hexa digits required to store the i32 code.
+ * returns u32 minimum number of hexa digits required to store the u32 code.
  */
-pub fn min_hexa_digits_required(code:i32)-> i32 {
-    return (code as f64).log(16.0) as i32 + 1;
+pub fn min_hexa_digits_required(code:u32)-> u32 {
+    return (code as f64).log(16.0) as u32 + 1;
 }
