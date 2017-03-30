4003030d872ab5754df7833c9c5272954a47b188
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2017-03-30 14:36:50 +0200

    Improve class design
        -   Add unwrap as vector for UnitOrPair
        -   Add compare variant for the operand
        -   Add more error checking before object code generation
        -   Add some tests for the instruction set

--- src/lib/pass_two/translator.rs
+++ src/lib/pass_two/translator.rs
@@ -2,7 +2,7 @@ use basic_types::flags::*;
 use basic_types::instruction::{Instruction, AsmOperand};
 use basic_types::operands::{OperandType, Value};
 use basic_types::formats::*;
-use basic_types::instruction_set;
+use basic_types::instruction_set::{self, AssemblyDef};
 use basic_types::unit_or_pair::UnitOrPair;
 use std::num;
 
@@ -11,21 +11,30 @@ pub fn translate(instruction: &Instruction) -> Result<u32, &str> {
     //resolve_instruction_code(instruction, 0).and_then(resolve_operands)
 
     // Check the flags for options
-    // Check for memory out of range error
+    // TODO: Check for memory out of range error, using the locctr of instruction
+    // TODO: check for base value
+
+    //validate_instruction().unwrap_or();
+
     // Assemble the instruciton
-    let raw_operans = resolve_incomplete_operands(instruction);
+    let raw_operands = resolve_incomplete_operands(instruction);
     let raw_opcode = resolve_opcode(instruction);
     let raw_flags = instruction.get_flags_value(); // TODO propagate the error from getting flag values
 
+
+
     debug!("Tranlating instruction {:?}", instruction);
-    debug!("Raw instruction operands {:?}", raw_operans);
+    debug!("Raw instruction operands {:?}", raw_operands);
     debug!("Raw flag value {:?}", raw_flags);
     debug!("Instruction opcode {:?}", raw_opcode);
 
-    unimplemented!();
+    // TODO: extract error message
+    // TODO: combine results
+
+    unimplemented!()
 }
 
-fn resolve_incomplete_operands(instruction: &Instruction) -> Result<Vec<u32>, String> {
+fn resolve_incomplete_operands(instruction: &Instruction) -> Result<Vec<u32>, &str> {
     // Convert immediate and indirect operands to a basic forms -> Raw
     let mut raws: Vec<u32> = Vec::new();
     let opVec = instruction.unwrap_operands();
@@ -41,26 +50,96 @@ fn resolve_incomplete_operands(instruction: &Instruction) -> Result<Vec<u32>, St
 
         match raw {
             Ok(x) => raws.push(x),
-            _ => return Err(format!("Couldn't resolve label {:?}", operand.val).clone()),  // Nothing else can fail
+            _ => return Err("Couldn't resolve label"),  // Nothing else can fail
         }
     }
     Ok(raws)
 }
 
+/// Get the opcode value from the instruction set table
 fn resolve_opcode(instr: &Instruction) -> Result<u32, &str> {
-    // Get the opcode value from the instruction set table
-    // Check format correctness
+
+    let instruction_set_def: AssemblyDef;
+
     match instruction_set::fetch_instruction(&instr.mnemonic) {
-        Ok(inst) => Ok(inst.op_code),
-        Err(err) => Err(err),
-    }
+        Ok(inst) => instruction_set_def = inst,
+        Err(err) => return Err(err),
+    };
+
+    Ok(instruction_set_def.op_code)
 }
 
+/// Fetch the label's address from the hash table
 fn resolve_label(label: &str) -> Result<u32, &str> {
-    // Check the symtab
-    // Check the range of addresses with the instruction format
+    // TODO: Check the symtab
+    // TODO: Check the range of addresses with the instruction format
     unimplemented!();
 }
 
-#[test]
-fn check_op_code() {}
+fn validate_instruction(instr: &Instruction) -> Result<(), &str> {
+    // TODO: aggregate errors
+    // TODO: indexed addressing with PC/Base relative instructions and for format 4
+    // Check format correctness
+    let instruction_set_def: AssemblyDef;
+
+    // Check mnemonic existence
+    match instruction_set::fetch_instruction(&instr.mnemonic) {
+        Ok(expr) => instruction_set_def = expr,
+        Err(e) => return Err(e),
+    }
+
+    // Check format correctness
+    if instruction_set_def.match_format(&instr.format) == false {
+        return Err("Formats mismatched");
+    }
+
+    // Check operands
+    if instruction_set_def.has_valid_operands(&instr.operands) == false {
+        return Err("Operands for this mnemonic are invalid");
+    }
+
+    // Check memory range
+
+    if instruction_set_def.match_format(&instr.format) == false {
+        return Err("Mismatched instruction formats");
+    }
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use basic_types::formats;
+    use basic_types::operands::Value;
+    use basic_types::register::Register;
+
+    #[test]
+    fn test_resolve_op_code() {
+        let mut inst = Instruction::new_simple("add".to_owned());
+        inst.format = formats::Format::Four;
+        assert_eq!(resolve_opcode(&inst).unwrap(), 0x18);
+
+        inst.format = formats::Format::Three;
+        assert!(resolve_opcode(&inst).unwrap() == 0x18);
+    }
+
+    #[test]
+    #[should_panic]
+    fn add_format_one() {
+        let mut inst = Instruction::new_simple("add".to_owned());
+
+        inst.format = formats::Format::One;
+        assert_eq!(resolve_opcode(&inst).unwrap(), 0x18);
+    }
+
+    #[test]
+    fn test_resolve_regs() {
+        let mut inst = Instruction::new_simple("add".to_owned());
+        inst.add_operand(OperandType::Register, Value::Register(Register::A));
+        let oprs: Vec<u32> = resolve_incomplete_operands(&inst).unwrap();
+
+        assert_eq!(oprs.len(), 1);
+        assert_eq!(oprs[0], 0);
+    }
+
+}
