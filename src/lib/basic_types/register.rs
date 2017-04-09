7d8504915169bcce29eaa82dfcb65d89f52a3969
oddcoder <ahmedsoliman@oddcoder.com>
2017-04-09 14:28:27 +0200

    fixing syntax error

--- src/lib/basic_types/register.rs
+++ src/lib/basic_types/register.rs
@@ -1,12 +1,8 @@
 /**
  * SIC/XE registers
  */
-<<<<<<< ead3018a2ce231ba32b1b3cf132be66c9b936cc2
 #[derive(Debug,PartialEq,Clone,Copy)]
 #[repr(u8)]
-=======
-#[derive(Debug,PartialEq)]
->>>>>>> Pass2 instruction translation (#4)
 pub enum Register {
     A = 0,
     X = 1,
