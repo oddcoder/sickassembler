df79831eb60f5586c691af4068021aeaaf34f337
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2017-04-10 22:44:04 +0200

    Remove unnecessary comments

--- src/lib/filehandler.rs
+++ src/lib/filehandler.rs
@@ -8,6 +8,7 @@ pub struct FileHandler {
     path: String,
     buf: BufReader<File>,
 }
+
 impl FileHandler {
     pub fn new(path: String) -> FileHandler {
         let file = File::open(&path).unwrap();
@@ -17,32 +18,35 @@ impl FileHandler {
             buf: f,
         };
     }
+
     pub fn read_instruction(&mut self) -> Option<String> {
         return self.scrap_comment();
     }
+
+    /// Removes comments if found in a line, and skips
+    /// empty lines.
     fn scrap_comment(&mut self) -> Option<String> {
         let mut line = String::new();
 
-        // Read until you reach end of file or a non-comment line
-        // if the line is just a blank line, skip it
         loop {
             match self.buf.read_line(&mut line) {
                 Ok(num) => {
                     if num == 0 {
-                        // Nothing Read
                         return None;
                     } else {
-                        // Remove the comments form the line read
                         line = line.split(".").nth(0).unwrap().trim().to_owned();
 
                         if line.is_empty() {
-                            continue; // Skip empty lines or lines that contain only comments
+                            continue;
                         }
 
                         return Some(line);
                     }
                 }
-                Err(e) => panic!(format!("An OS I/O error occured, this is really bad!, {}",e.to_string())),
+                Err(e) => {
+                    panic!(format!("An OS I/O error occured, this is really bad!, {}",
+                                   e.to_string()))
+                }
             }
 
         }
