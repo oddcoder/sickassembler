baad8c726b5e80668bc49a12178a39adff9f1728
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2017-04-15 19:32:49 +0200

    String object code for output

--- src/lib/pass_two/translator.rs
+++ src/lib/pass_two/translator.rs
@@ -1,14 +1,9 @@
-use basic_types::flags::*;
-use basic_types::instruction::{Instruction, AsmOperand};
-use basic_types::operands::{OperandType, Value};
-use basic_types::formats::*;
+use basic_types::instruction::Instruction;
+use basic_types::operands::Value;
 use basic_types::instruction_set::{self, AssemblyDef};
-<<<<<<< Updated upstream
-use basic_types::unit_or_pair::UnitOrPair;
-use std::num;
-=======
+use std::fmt::UpperHex;
+use std::marker::Sized;
 use regex::Regex;
->>>>>>> Stashed changes
 
 pub fn translate(instruction: &Instruction) -> Result<u32, &str> {
     //let f_vals = instruction.check_invalid_flags();   // TODO Report to RLS
@@ -38,32 +33,41 @@ pub fn translate(instruction: &Instruction) -> Result<u32, &str> {
     unimplemented!()
 }
 
-fn resolve_incomplete_operands(instruction: &Instruction) -> Result<Vec<u32>, &str> {
+fn resolve_incomplete_operands(instruction: &Instruction) -> Result<String, &str> {
     // Convert immediate and indirect operands to a basic forms -> Raw
-    let mut raws: Vec<u32> = Vec::new();
+    let mut raws: String = String::new();
     let op_vec = instruction.unwrap_operands();
 
     for operand in &op_vec {
-        let raw: Result<Vec<u32>, &str> = match operand.val {
-            Value::SignedInt(x) => Ok(vec![x.abs() as u32]),
-            Value::Register(ref x) => Ok(vec![(*x as u8) as u32]),
+        let raw: Result<String, &str> = match operand.val {
+            Value::SignedInt(x) => {
+                if x > 0x7FFFFF {
+                    return Err("Value out of 23-bit range");
+                }
+                Ok(to_hex(x))
+            }
+            Value::Register(ref x) => {
+                let reg_num = *x as u8;
+                Ok((reg_num as u32).to_string())
+            }
             // Get from symtab
-            Value::Label(ref x) => resolve_label(x.as_str()).and_then(|v| Ok(vec![v])),
-            Value::Raw(x) => Ok(vec![x]),
+            Value::Label(ref x) => resolve_label(x.as_str()),
+            Value::Raw(x) => Ok(x.to_string()),
+            // Used by WORD / BYTE -> Generate hex codes for operand
             Value::Bytes(ref text) => resolve_directive_operand(text),
         };
 
         if raw.is_err() {
             return Err("Couldn't resolve label"); // Nothing else can fail
         }
-        let mut operand: Vec<u32> = raw.unwrap();
-        raws.append(&mut operand);
+        let mut operand: String = raw.unwrap();
+        raws.push_str(&mut operand);
     }
     Ok(raws)
 }
 
 /// Get the opcode value from the instruction set table
-fn resolve_opcode(instr: &Instruction) -> Result<u32, &str> {
+fn resolve_opcode(instr: &Instruction) -> Result<String, &str> {
 
     let instruction_set_def: AssemblyDef;
 
@@ -72,32 +76,47 @@ fn resolve_opcode(instr: &Instruction) -> Result<u32, &str> {
         Err(err) => return Err(err),
     };
 
-    Ok(instruction_set_def.op_code)
+    Ok(to_hex(instruction_set_def.op_code))
 }
 
-fn resolve_label(label: &str) -> Result<u32, &str> {
+fn resolve_label(label: &str) -> Result<String, &str> {
     // TODO: Check the symtab
     // TODO: Check the range of addresses with the instruction format
     unimplemented!();
 }
 
-fn resolve_directive_operand(operand: &String) -> Result<Vec<u32>, &str> {
-    let re: Regex = Regex::new(r"^(C|X)'.+'").unwrap();
-    if re.is_match(operand) == false {
+/// Converts the operand of the WORD/BYTE directive to object code
+fn resolve_directive_operand(operand: &String) -> Result<String, &str> {
+    // TODO: lazy_static the regex
+    let hex_regex: Regex = Regex::new(r"^(x|X)'[0-9a-fA-F]+'").unwrap();
+    let str_regex: Regex = Regex::new(r"^(c|C)'.+'").unwrap();
+
+    if hex_regex.is_match(operand) == false && str_regex.is_match(operand) == false {
         return Err("Operand isn't on the correct format");
     }
 
-    let operand = operand.clone().to_uppercase();
-    if operand.starts_with('X') {
-        // TODO: Hex value
-
-    } else if operand.starts_with('C') {
-        // TODO: String value, convert it to bytes
-
+    if operand.starts_with('X') || operand.starts_with('x') {
+        // ex. INPUT BYTE X’F1’ -> F1
+        let captures = hex_regex.captures(operand.as_str()).unwrap();
+        let mut operand_match: String = captures.get(0).unwrap().as_str().to_owned();
+        remove_container(&mut operand_match);
+        return Ok(operand_match);
     } else {
-        return Err("Operand type undefined");
+        let captures = str_regex.captures(operand.as_str()).unwrap();
+        let mut operand_match: String = captures.get(0).unwrap().as_str().to_owned();
+        remove_container(&mut operand_match);
+
+        return Ok(parse_str_operand(operand_match));
     }
-    unimplemented!()
+}
+
+fn parse_str_operand(operand_match: String) -> String {
+    // EOF BYTE C’EOF’ -> 454F46
+    operand_match.chars()
+        .map(|c| to_hex(c as u32))
+        .collect::<Vec<String>>()
+        .join("")
+
 }
 
 fn validate_instruction(instr: &Instruction) -> Result<(), &str> {
@@ -130,37 +149,43 @@ fn validate_instruction(instr: &Instruction) -> Result<(), &str> {
     unimplemented!()
 }
 
+/// Removes the container of a WORD/BYTE oeprand, the prefix, the '
+/// X'asdas' -> asdas ,and so on
+fn remove_container(byte_operand: &mut String) {
+    byte_operand.remove(0);
+    byte_operand.remove(0);
+    byte_operand.pop();
+}
+
+fn to_hex<T>(num: T) -> String
+    where T: UpperHex + Sized
+{
+    format!("{:X}", num)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use basic_types::formats;
-    use basic_types::operands::Value;
+    use basic_types::operands::{Value, OperandType};
     use basic_types::register::Register;
 
     #[test]
     fn test_resolve_op_code() {
         let mut inst = Instruction::new_simple("add".to_owned());
         inst.format = formats::Format::Four;
-        assert_eq!(resolve_opcode(&inst).unwrap(), 0x18);
+        assert_eq!(resolve_opcode(&inst).unwrap(), "18");
 
         inst.format = formats::Format::Three;
-        assert!(resolve_opcode(&inst).unwrap() == 0x18);
-    }
-
-    #[test]
-    fn add_format_one() {
-        let mut inst = Instruction::new_simple("add".to_owned());
-        assert_eq!(resolve_opcode(&inst).unwrap(), 0x18);
+        assert!(resolve_opcode(&inst).unwrap() == "18");
     }
 
     #[test]
     fn test_resolve_regs() {
         let mut inst = Instruction::new_simple("add".to_owned());
         inst.add_operand(OperandType::Register, Value::Register(Register::A));
-        let oprs: Vec<u32> = resolve_incomplete_operands(&inst).unwrap();
-
-        assert_eq!(oprs.len(), 1);
-        assert_eq!(oprs[0], 0);
+        let opr: String = resolve_incomplete_operands(&inst).unwrap();
+        assert_eq!(opr, "0");
     }
 
     #[test]
@@ -182,26 +207,25 @@ mod tests {
     }
 
     #[test]
-    fn test_byte_operand_passing() {
-        let test_str: String = "C'abc'".to_owned(); // Malformed
-        let test_str_1: String = "X'FF'".to_owned(); // Malformed
-        let test_str_2: String = "X'0A'".to_owned(); // Malformed
-        let test_str_3: String = "C'abcdefg'".to_owned(); // Malformed
+    fn test_byte_operand_parsing() {
 
-        let result = resolve_directive_operand(&test_str);
-        let result_1 = resolve_directive_operand(&test_str_1);
-        let result_2 = resolve_directive_operand(&test_str_1);
-        let result_3 = resolve_directive_operand(&test_str_1);
+        check_str_operand("x'0A'", "0A");
+        check_str_operand("x'FF'", "FF");
 
-        assert!(result.is_ok());
-        assert!(result_1.is_ok());
-        assert!(result_2.is_ok());
-        assert!(result_3.is_ok());
-
-        assert_eq!(result, vec![97, 98, 99]);
-        assert_eq!(result_1, 0xFF);
-        assert_eq!(result_2, 0x0A);
-        assert_eq!(result_3, vec![97, 98, 99, 100, 101, 102, 103]);
+        check_str_operand("C'cab'", "636162");
+        check_str_operand("C'EOF'", "454F46");
     }
 
+    fn check_str_operand(x: &str, v: &str) {
+        let result = resolve_directive_operand(&x.to_owned())
+            .expect(format!("Failed to parse {}", x).as_str());
+        assert_eq!(result.to_uppercase(), v.to_uppercase());
+    }
+
+    #[test]
+    fn check_string_convert_to_vec() {
+        let test_str: String = "abc".to_owned();
+        let result = parse_str_operand(test_str);
+        assert_eq!(result, "616263");
+    }
 }
