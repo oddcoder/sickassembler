ce9b2c05ea4d02884e5ae1497bc05a703920953e
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2017-04-20 22:22:33 +0200

    Add literal table

---
+++ src/lib/basic_types/literal.rs
@@ -0,0 +1,30 @@
+use std::hash::{Hash, Hasher};
+use std::cmp::{Eq, PartialEq};
+
+#[derive(Debug,Clone)]
+pub struct Literal {
+    pub name: String,
+    pub value: String, // Object code value
+    pub external_name: String, // Value in code, ex. C'EOF'
+    pub address: u32,
+}
+
+impl Literal {
+    fn length_in_bytes(&self) -> u32 {
+        return (self.value.len() / 2) as u32;
+    }
+}
+
+impl Hash for Literal {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.value.hash(state);
+    }
+}
+
+impl PartialEq for Literal {
+    fn eq(&self, other: &Literal) -> bool {
+        self.value == other.value
+    }
+}
+
+impl Eq for Literal {}
---
+++ src/lib/basic_types/literal_table.rs
@@ -0,0 +1,78 @@
+use std::collections::HashSet;
+use std::iter::Iterator;
+use parking_lot::RwLock;
+use std::ops::DerefMut;
+use pass_two::translator::translate_literal;
+use basic_types::literal::Literal;
+
+
+
+lazy_static!{
+    static ref LITERAL_TABLE: RwLock<HashSet<Literal>> = RwLock::new(HashSet::new());
+    static ref TEMP_LITERALS: RwLock<HashSet<String>> = RwLock::new(HashSet::new());    
+    static ref LIT_ID: RwLock<u32> = RwLock::new(0);
+}
+
+fn insert_literal(literal: String, address: u32) {
+    let lit: Literal;
+    {
+        let mut temp = LIT_ID.write();
+        let mut literal_id: &mut u32 = temp.deref_mut();
+        lit = Literal {
+            name: "lit_".to_owned() + &literal_id.to_string(),
+            value: translate_literal(&literal),
+            external_name: literal,
+            address: address,
+        };
+        *literal_id = *literal_id + 1;
+    }
+
+    {
+        LITERAL_TABLE.write().insert(lit);
+    }
+}
+
+/// Insert a literal name to the temp literal table
+fn insert_unresolved(literal_name: String) {
+    TEMP_LITERALS.write().insert(literal_name);
+}
+
+/// Called when encountering LTORG or end of file
+fn get_unresolved() -> Vec<String> {
+    let ret: Vec<String> =
+        TEMP_LITERALS.read().iter().map(|li| li.to_owned()).collect::<Vec<String>>();
+    TEMP_LITERALS.write().clear();
+    ret
+}
+
+fn get_literal(name: &String) -> Option<Literal> {
+    println!("Table");
+    let val: String;
+    {
+        println!("La Tabolo");
+        val = translate_literal(name);
+        println!("La Tabola");
+    }
+    println!("La Table");
+    //println!("LIT Count: {:?}", LITERAL_TABLE.read().keys().len());
+    let table = LITERAL_TABLE.read();
+
+    for lit in table.iter() {
+        println!("LIT: {:?} --- {:?}", lit, name);
+        if *lit.value == val || lit.name == *name {}
+        return Some(lit.clone());
+    }
+    None
+}
+
+#[test]
+fn add_get_literal() {
+    insert_unresolved("C'EOF'".to_owned());
+
+    for s in get_unresolved().iter() {
+        insert_literal(s.to_owned(), 45);
+    }
+
+    assert!(get_literal(&"C'EOF'".to_owned()).is_some());
+    assert!(get_literal(&"X'454F46'".to_owned()).is_some());
+}
