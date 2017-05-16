ce6849eff9cd67f708f8cbf25b1931d88f6cc3a2
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2017-05-16 19:29:55 +0200

    Add symbol type

---
+++ src/lib/basic_types/symbol.rs
@@ -0,0 +1,60 @@
+use std::hash::{Hash, Hasher};
+use std::cmp::{PartialEq, Eq};
+
+#[derive(Debug)]
+pub struct Symbol {
+    name: String,
+    address: i32,
+    control_section: String,
+}
+
+impl Symbol {
+    pub fn new(name: String, addr: i32, csect: String) -> Symbol {
+        let mut csect = csect;
+        if csect.is_empty() {
+            csect = "#DEFAULT#".to_owned();
+        }
+        Symbol {
+            name: name,
+            address: addr,
+            control_section: csect,
+        }
+    }
+
+    pub fn set_address(&mut self) {}
+
+    pub fn get_address(&self) -> i32 {
+        self.address
+    }
+
+    pub fn get_name(&self) -> String {
+        self.name.clone()
+    }
+
+    pub fn get_control_section(&self) -> String {
+        self.control_section.clone()
+    }
+}
+
+impl Clone for Symbol {
+    fn clone(&self) -> Symbol {
+        Symbol::new(self.name.clone(),
+                    self.address,
+                    self.control_section.clone())
+    }
+}
+
+impl Hash for Symbol {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        // Hash with both control section and name
+        self.name.hash(state);
+        self.control_section.hash(state);
+    }
+}
+
+impl PartialEq for Symbol {
+    fn eq(&self, other: &Symbol) -> bool {
+        self.name == other.name && self.control_section == other.control_section
+    }
+}
+impl Eq for Symbol {}
